5c7ba820d37ca75f142d2cbdd7b43d9d109973b6
Huns <studyjung@gmail.com>
2026-04-20 10:27:32 +0900

    Update CodeReview_02.bat

--- bin/CodeReview_02.bat
+++ bin/CodeReview_02.bat
@@ -3,10 +3,11 @@
 
 set "CLD=D:/SRC/CodeReview"
 set "BIN=D:/SRC/CodeReview/bin"
-set "CODEX_CMD=%USERPROFILE%/AppData/Roaming/npm/codex.cmd"
+::set "CODEX_CMD=%USERPROFILE%/AppData/Roaming/npm/codex.cmd"
 
 cd /d %CLD%
 
 @echo "CodeReview_02"
 
 claude --dangerously-skip-permissions --model claude-sonnet-4-6 --effort max "%BIN%/CodeReview_02.md Execute"
+::type "%BIN%/CodeReview_02.md" | codex exec --yolo -m gpt-5.4 "Execute"
