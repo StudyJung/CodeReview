b2824329f86e1b26e74c2f8aab3ad5a200ef6db2
Huns <studyjung@gmail.com>
2026-04-30 10:29:11 +0900

    Update CodeReview.bat

--- exe/CodeReview.bat
+++ exe/CodeReview.bat
@@ -27,11 +27,11 @@ claude --dangerously-skip-permissions --model %MODEL% --effort max -p "%BIN%/Cod
 
 if errorlevel 1 (
     @echo "Codex"
-    codex exec --yolo -m gpt-5.5 "Execute" < "%BIN%\CodeReview_01.md" >nul 2>&1
-	codex exec --yolo -m gpt-5.5 "Execute" < "%BIN%\CodeReview_02.md" >nul 2>&1
-	codex exec --yolo -m gpt-5.5 "Execute" < "%BIN%\CodeReview_03.md" >nul 2>&1
+    codex exec --yolo -m gpt-5.5 -c model_reasoning_effort=xhigh "Execute" < "%BIN%\CodeReview_01.md" >nul 2>&1
+	codex exec --yolo -m gpt-5.5 -c model_reasoning_effort=xhigh "Execute" < "%BIN%\CodeReview_02.md" >nul 2>&1
+	codex exec --yolo -m gpt-5.5 -c model_reasoning_effort=xhigh "Execute" < "%BIN%\CodeReview_03.md" >nul 2>&1
 ) else (
 	@echo "Claude"
-	claude --dangerously-skip-permissions --model claude-sonnet-4-6 --effort max -p "%BIN%/CodeReview_02.md Execute"
-	claude --dangerously-skip-permissions --model claude-sonnet-4-6 --effort high -p "%BIN%/CodeReview_03.md Execute"
+	claude --dangerously-skip-permissions --model claude-opus-4-7 --effort max -p "%BIN%/CodeReview_02.md Execute"
+	claude --dangerously-skip-permissions --model claude-opus-4-7 --effort max -p "%BIN%/CodeReview_03.md Execute"
 )
